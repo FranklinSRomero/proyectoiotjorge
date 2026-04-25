b24bb2605d8105f08d53880e2f05e6b03f42ab65
Franklin Fernandez <113615053+FranklinSRomero@users.noreply.github.com>
2026-04-25 04:48:54 +0000

    rhtgfded

--- recibir_datos.php
+++ recibir_datos.php
@@ -1,17 +1,10 @@
- <?php
- $ppm = $_POST['ppm'];
- $conexion = mysqli_connect("sql107.infinityfree.com", "if0_41749531", "OQJYHljsnukme", "if0_41749531_monitor_gases");
- 
- $sql = "INSERT INTO lecturas (valor_gas, fecha_hora) VALUES ($ppm, NOW())";
- mysqli_query($conexion, $sql);
- 
- // Enviar alerta si el nivel es alto
-if ($ppm > 400) {
-   $para = "alertas@tucorreo.com";
-   $asunto = "⚠️ Alerta de CO₂";
-   $mensaje = "Nivel peligroso de CO₂ detectado: $ppm ppm. Verifica la zona inmediatamente.";
-   $cabeceras = "From: sistema@monitor.com";
-
-   mail($para, $asunto, $mensaje, $cabeceras);
+<?php
+if (!isset($_POST['ppm'])) {
+    http_response_code(400);
+    exit("No llegó ppm");
 }
+
+$ppm = $_POST['ppm'];
+
+echo "Render funcionando. PPM recibido: " . $ppm;
 ?>
